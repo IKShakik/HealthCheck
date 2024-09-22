c43071112ff206911805ea7a020a58f299499632
Ibrahim Khalil Shakik <shakik.social@gmail.com>
2024-09-22 15:58:05 +0600

    Chapter-3: Looking Around

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,18 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
-
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,11 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
-
 app.Run();
