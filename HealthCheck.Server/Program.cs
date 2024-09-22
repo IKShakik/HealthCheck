8082d36f1e869a3fe8a034e880d85c3598d90102
Ibrahim Khalil Shakik <shakik.social@gmail.com>
2024-09-22 22:00:47 +0600

    Chapter-5: Front-End and Back-End Interactions

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,7 +1,16 @@
+global using HealthCheck.Server;
+
+
 
 
 // Add services to the container.
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+   .AddCheck("ICMP_01",new ICMPHealthCheck("www.ryadel.com", 100))
+   .AddCheck("ICMP_02",new ICMPHealthCheck("www.google.com", 100))
+.AddCheck("ICMP_03",new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +42,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 app.MapControllers();
 app.MapFallbackToFile("/index.html");
 app.Run();
